3edc0a85d629f1016866b2ba3582abaa0b836be0
kousuke nezu <kousuke.n2132@gmail.com>
2017-03-02 21:08:53 +0900

    [add] 必要なファイルの作成

---
+++ wear/src/main/java/com/example/cyder/aktivaturbo/CircleGauge.java
@@ -0,0 +1,23 @@
+package com.example.cyder.aktivaturbo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by kousuke nezu on 2017/03/02.
+ */
+
+public class CircleGauge extends View {
+	public CircleGauge(Context context) {
+		super(context);
+	}
+
+	public CircleGauge(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
+
+	public CircleGauge(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+}
