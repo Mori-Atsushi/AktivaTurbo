e3b77524794c0d4316178b144c68545bd8beeb66
kousuke nezu <kousuke.n2132@gmail.com>
2017-03-03 23:20:46 +0900

    [add] サークルゲージの情報を保持するフィールド変数の追加

--- wear/src/main/java/com/example/cyder/aktivaturbo/CircleGauge.java
+++ wear/src/main/java/com/example/cyder/aktivaturbo/CircleGauge.java
@@ -1,6 +1,7 @@
 package com.example.cyder.aktivaturbo;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -9,6 +10,19 @@ import android.view.View;
  */
 
 public class CircleGauge extends View {
+	/** 文章 */
+	private String text;
+	/** textの文字色(デフォルトは白) */
+	private int textColor = Color.WHITE;
+	/** ゲージ自体の背景色 */
+	private int gaugeBackgroundColor;
+	/** ゲージ自体の前景色 */
+	private int gaugeForegroundColor;
+	/** ゲージの半径(ゲージの一番外側の円を半径とします) */
+	private int gaugeRadius;
+	/** ゲージの太さ */
+	private int gaugeWidth;
+
 	public CircleGauge(Context context) {
 		super(context);
 	}
