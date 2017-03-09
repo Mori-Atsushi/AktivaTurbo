d56325c3c34890e752b7118072b325d49363006b
kousuke nezu <kousuke.n2132@gmail.com>
2017-03-09 21:26:09 +0900

    [update] スマートな書き方に変更
    ただし四角形のwearにしか適用せず

--- wear/src/main/java/com/example/cyder/aktivaturbo/MainActivity.java
+++ wear/src/main/java/com/example/cyder/aktivaturbo/MainActivity.java
@@ -5,37 +5,42 @@ import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity {
 
+	/** レイアウト */
+	private RelativeLayout relativeLayout;
     /** 再生ボタン */
     private ImageButton playButton;
+	/** 速度ゲージ */
+	private CircleGauge circleGauge;
     /** 再生ボタンの状態をあらわす。ture=再生、false=停止 */
     private boolean isPlaying = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            /** この関数の内部でWatchViewStub内のViewの初期化*/
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-//                ViewのIDを検索
-                playButton = (ImageButton)findViewById(R.id.play_button);
-//                押されている状態か否かを設定
-                playButton.setActivated(isPlaying);
-//                押されたときの挙動
-                playButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        isPlaying = !isPlaying;
-                        v.setActivated(isPlaying);
-                    }
-                });
-            }
-        });
+        setContentView(R.layout.rect_activity_main);
+//		RelativeLayoutの初期化
+		relativeLayout = (RelativeLayout)findViewById(R.id.rect_relativelayout);
+//		ViewのIDを検索
+		playButton = (ImageButton)findViewById(R.id.play_button);
+//		押されている状態か否かを設定
+		playButton.setActivated(isPlaying);
+//      押されたときの挙動
+		playButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				isPlaying = !isPlaying;
+				v.setActivated(isPlaying);
+			}
+		});
+		circleGauge = (CircleGauge)findViewById(R.id.speed_circlegauge);
     }
+
+    @Override
+    protected void onResume(){
+		super.onResume();
+	}
 }
